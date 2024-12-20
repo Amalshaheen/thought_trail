56e97ff30d132a425b8fc3d79da7e5216ed44db0
Amal Shaheen <mkamalshaheen@gmail.com>
2024-12-20 16:22:16 +0530

    added theme data for light and dark theme

--- lib/main.dart
+++ lib/main.dart
@@ -12,9 +12,24 @@ class NewApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: const ColorScheme.light(
+          primary: Color(0xFF704214), // Rich sepia brown
+          surface: Color(0xFFF4EAD5), // Creamy beige
+          onPrimary: Color(0xFFFFFFFF), // Text/icons on sepia
+          onSurface: Color(0xFF4A4A4A), // Dark gray for labels/icons
+          brightness: Brightness.light,
+        ),
         useMaterial3: true,
       ),
+      darkTheme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor:
+              Color(0xFF00FFFF), // Same seed color, but adapted for dark mode
+          brightness: Brightness.dark, // Ensure it's a dark theme
+        ),
+        useMaterial3: true,
+      ),
+      themeMode: ThemeMode.light,
       home: const HomeScreen(),
     );
   }
@@ -25,8 +40,42 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.white,
+    return Scaffold(
+      appBar: AppBar(title: Text('Appbar')),
+      body: Column(
+        children: [
+          Text("This is just a sample"),
+          ElevatedButton.icon(
+            onPressed: () {},
+            label: Text('Click me'),
+            icon: Icon(Icons.home),
+          ),
+          OutlinedButton(
+            onPressed: () {},
+            child: Text('Outlined button'),
+          ),
+          TextButton(
+            onPressed: () {},
+            child: Text('Text button'),
+          ),
+          Switch(
+            value: true,
+            onChanged: (value) {},
+          ),
+          Checkbox(
+            value: true,
+            onChanged: (value) {},
+          ),
+          Radio(
+            value: 1,
+            groupValue: 1,
+            onChanged: (value) {},
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+      ),
     );
   }
 }
