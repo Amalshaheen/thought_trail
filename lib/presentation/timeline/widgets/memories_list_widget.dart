d8c80dba32708b5483ea33012c525bf3868cf03b
Amal Shaheen <mkamalshaheen@gmail.com>
2024-12-27 10:01:06 +0530

    changed filename

---
+++ lib/presentation/timeline/widgets/memories_list_widget.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:thought_trail/presentation/timeline/widgets/memory_list_tile_widget.dart';
+
+import '../../../dev/test_memories_generator.dart';
+
+class MemoriesListWidget extends StatelessWidget {
+  const MemoriesListWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final memories = generateTestMemories();
+
+//
+//
+    return SliverList.builder(
+      // physics: BouncingScrollPhysics(),
+      // padding: EdgeInsets.only(
+      //   right: 10,
+      //   left: 10,
+      //   top: 10,
+      // ),
+      // shrinkWrap: true,
+      itemCount: memories.length,
+      itemBuilder: (context, index) {
+        return MemoryListTileWidget(
+          memory: memories[index],
+        );
+      },
+    );
+  }
+}
