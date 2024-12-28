7f4a687c6e766a79d900e5f6c58fb7c70b829a08
Amal Shaheen <mkamalshaheen@gmail.com>
2024-12-28 21:31:06 +0530

    design of memory entry bottom modal sheet with FAB

--- lib/presentation/timeline/timeline_page.dart
+++ lib/presentation/timeline/timeline_page.dart
@@ -19,38 +19,171 @@ class TimelinePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: CustomScrollView(
-        physics: BouncingScrollPhysics(),
         slivers: [
           SliverAppBar(
             title: Text('ThoughtTrail'),
             floating: true,
             snap: true,
+            actions: [
+              IconButton(
+                onPressed: () {
+                  final theme = Theme.of(context);
+                  final isDark = theme.brightness == Brightness.dark;
+                  final newTheme = isDark ? lightThemeData() : darkThemeData();
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(
+                      content: Text(isDark
+                          ? 'Switched to light mode'
+                          : 'Switched to dark mode'),
+                    ),
+                  );
+                  Navigator.of(context).pushReplacement(
+                    MaterialPageRoute(
+                      builder: (context) => MaterialApp(
+                        theme: newTheme,
+                        home: const TimelinePage(),
+                      ),
+                    ),
+                  );
+                },
+                icon: Theme.of(context).brightness == Brightness.light
+                    ? Icon(Icons.light_mode)
+                    : Icon(Icons.dark_mode),
+              ),
+            ],
           ),
           MemoriesListWidget(),
         ],
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          final theme = Theme.of(context);
-          final isDark = theme.brightness == Brightness.dark;
-          final newTheme = isDark ? lightThemeData() : darkThemeData();
-          ScaffoldMessenger.of(context).showSnackBar(
-            SnackBar(
-              content: Text(
-                  isDark ? 'Switched to light mode' : 'Switched to dark mode'),
-            ),
-          );
-          Navigator.of(context).pushReplacement(
-            MaterialPageRoute(
-              builder: (context) => MaterialApp(
-                theme: newTheme,
-                home: const TimelinePage(),
-              ),
-            ),
+          showModalBottomSheet(
+            context: context,
+            builder: (context) {
+              ValueNotifier<DateTime> date = ValueNotifier(DateTime.now());
+              return Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text('Add Your Memory Here'),
+                    SizedBox(height: 10),
+                    Row(
+                      children: [
+                        Text('Date: '),
+                        TextButton(
+                            onPressed: () {
+                              showDatePicker(
+                                context: context,
+                                initialDate: date.value,
+                                firstDate: DateTime(2000),
+                                lastDate: DateTime(2100),
+                              ).then((value) {
+                                if (value != null) {
+                                  date.value = date.value.copyWith(
+                                    year: value.year,
+                                    month: value.month,
+                                    day: value.day,
+                                  );
+                                }
+                              });
+                            },
+                            child: ValueListenableBuilder(
+                                valueListenable: date,
+                                builder: (context, value, child) {
+                                  return Text(
+                                      '${value.day}/${value.month}/${value.year}');
+                                })),
+                      ],
+                    ),
+
+                    SizedBox(height: 10),
+                    //time
+                    Row(
+                      children: [
+                        Text('Time: '),
+                        TextButton(
+                          onPressed: () {
+                            showTimePicker(
+                              context: context,
+                              initialTime: TimeOfDay.now(),
+                            ).then((value) {
+                              if (value != null) {
+                                date.value = DateTime(
+                                  date.value.year,
+                                  date.value.month,
+                                  date.value.day,
+                                  value.hour,
+                                  value.minute,
+                                );
+                              }
+                            });
+                          },
+                          child: ValueListenableBuilder(
+                            builder: (context, value, child) {
+                              return Text(
+                                  '${value.hour.toString().padLeft(2, '0')}:${value.minute.toString().padLeft(2, '0')}');
+                            },
+                            valueListenable: date,
+                          ),
+                        ),
+                      ],
+                    ),
+
+                    SizedBox(height: 10),
+                    MemoryEntryWidget(),
+                    SizedBox(height: 10),
+
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        ElevatedButton(
+                          onPressed: () {
+                            // Handle memory entry submission
+                            Navigator.pop(context);
+                          },
+                          child: Text('Cancel'),
+                        ),
+                        ElevatedButton(
+                          onPressed: () {
+                            // Handle memory entry submission
+                            Navigator.pop(context);
+                          },
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor:
+                                Theme.of(context).colorScheme.primary,
+                            foregroundColor:
+                                Theme.of(context).colorScheme.onPrimary,
+                          ),
+                          child: Text('Add Memory'),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              );
+            },
           );
         },
-        child: const Icon(Icons.palette),
+        child: const Icon(Icons.add),
       ),
+    );
+  }
+}
+
+class MemoryEntryWidget extends StatelessWidget {
+  const MemoryEntryWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      decoration: InputDecoration(
+        hintText: 'Enter your memory here',
+        border: OutlineInputBorder(),
+      ),
+      maxLines: 5,
     );
   }
 }
