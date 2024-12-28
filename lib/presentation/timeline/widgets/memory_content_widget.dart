31ce7f3bcd84e9fda0a6cadc28a73e7dfc97056f
Amal Shaheen <mkamalshaheen@gmail.com>
2024-12-28 21:28:00 +0530

    minor tweaks for image widget

--- lib/presentation/timeline/widgets/memory_content_widget.dart
+++ lib/presentation/timeline/widgets/memory_content_widget.dart
@@ -13,11 +13,27 @@ class MemoryContentWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     // return Text('${memory.memory!.text}');
     return memoryContent.map(
-      text: (text) => Text(text.text),
+      text: (text) => Text(
+        text.text,
+      ),
       image: (image) => Padding(
         padding: const EdgeInsets.all(8.0),
         child: Image.network(
+          height: 200,
           image.imagePath,
+          frameBuilder: (context, child, frame, wasSynchronouslyLoaded) {
+            // print('image loaded');
+            if (wasSynchronouslyLoaded) {
+              return child;
+            } else {
+              return AnimatedOpacity(
+                opacity: frame == null ? 0 : 1,
+                duration: const Duration(seconds: 1),
+                curve: Curves.easeOut,
+                child: child,
+              );
+            }
+          },
         ),
       ),
       audio: (value) => Container(height: 50, color: Colors.red[300]),
