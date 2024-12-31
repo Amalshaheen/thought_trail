d53778b5db07b253ffff92e419d448a463bee55d
Amal Shaheen <mkamalshaheen@gmail.com>
2024-12-31 21:31:17 +0530

    implemented non-empty string validator function

---
+++ lib/domain/core/value_validators.dart
@@ -0,0 +1,10 @@
+import 'package:dartz/dartz.dart';
+import 'package:thought_trail/domain/core/failures.dart';
+
+Either<ValueFailure<String>, String> validateStringNotEmpty(String input) {
+  if (input.isNotEmpty) {
+    return right(input);
+  } else {
+    return left(ValueFailure.empty(failedValue: input));
+  }
+}
