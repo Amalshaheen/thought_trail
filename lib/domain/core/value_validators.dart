c9e8c32092a09fd81c1daf2ded93e1bb49a46ee1
Amal Shaheen <mkamalshaheen@gmail.com>
2025-01-01 22:03:17 +0530

    added more validators for image and audio

--- lib/domain/core/value_validators.dart
+++ lib/domain/core/value_validators.dart
@@ -8,3 +8,22 @@ Either<ValueFailure<String>, String> validateStringNotEmpty(String input) {
     return left(ValueFailure.empty(failedValue: input));
   }
 }
+
+Either<ValueFailure<String>, String> validateURL(String input) {
+  final regex =
+      r'((http|https|ftp|file)://)(www.)?[a-zA-Z0-9@:%._\\+~#?&//=]{2,256}\\.[a-z]{2,6}\\b([-a-zA-Z0-9@:%._\\+~#?&//=]*)';
+  if (RegExp(regex).hasMatch(input)) {
+    return right(input);
+  } else {
+    return left(ValueFailure.invalidURL(failedValue: input));
+  }
+}
+
+Either<ValueFailure<String>, String> validateAudioURL(String input) {
+  final regex = r'((http|https|ftp|file)://).*(.mp3|.wav|.ogg)';
+  if (RegExp(regex).hasMatch(input)) {
+    return right(input);
+  } else {
+    return left(ValueFailure.invalidAudioURL(failedValue: input));
+  }
+}
