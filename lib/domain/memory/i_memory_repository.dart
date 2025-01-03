abe6a7bca8953af49b419f94938ee9bccb9a419e
Amal Shaheen <mkamalshaheen@gmail.com>
2025-01-03 21:00:54 +0530

    changed return type to EIther for better handling

--- lib/domain/memory/i_memory_repository.dart
+++ lib/domain/memory/i_memory_repository.dart
@@ -1,7 +1,16 @@
+import 'package:dartz/dartz.dart';
+import 'package:thought_trail/domain/core/failures.dart';
 import 'package:thought_trail/domain/memory/memory.dart';
 
 abstract class IMemoryRepository {
-  Future<void> saveMemory(Memory memory);
-  Future<void> deleteMemory(Memory memory);
-  Future<List<Memory>> getMemories();
+  Future<Either<ValueFailure, Unit>> create({
+    required Memory memory,
+  });
+  Future<Either<ValueFailure, Unit>> delete({
+    required Memory memory,
+  });
+  Future<Either<ValueFailure, Unit>> update({
+    required Memory memory,
+  });
+  Future<Either<ValueFailure, List<Memory>>> getAll();
 }
