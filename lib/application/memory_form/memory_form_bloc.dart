32b40e4326fe06eaf8657f413c9915ad6be713ea
Amal Shaheen <mkamalshaheen@gmail.com>
2025-01-09 21:12:37 +0530

    added MemoryFormBloc Initialized Event BLOC

--- lib/application/memory_form/memory_form_bloc.dart
+++ lib/application/memory_form/memory_form_bloc.dart
@@ -4,7 +4,6 @@ import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:thought_trail/domain/core/failures.dart';
 import 'package:thought_trail/domain/memory/memory.dart';
 import 'package:thought_trail/domain/memory/memory_content.dart';
-import 'package:thought_trail/domain/memory/value_objects.dart';
 
 part 'memory_form_event.dart';
 part 'memory_form_state.dart';
@@ -12,58 +11,46 @@ part 'memory_form_bloc.freezed.dart';
 
 class MemoryFormBloc extends Bloc<MemoryFormEvent, MemoryFormState> {
   MemoryFormBloc() : super(MemoryFormState.initial()) {
-    on<_Initialized>(
-      (event, emit) => emit(
-        event.initialMemoryOption.fold(
-          () => state,
-          (initialMemory) => state.copyWith(
-            memory: initialMemory,
-            isEditing: true,
-          ),
+    on<_Initialized>((event, emit) {
+      emit(event.initialMemoryOption.fold(
+        () => state.copyWith(
+          failureOrSuccessOption: none(),
         ),
-      ),
-    );
-
-    on<_DateTimeChanged>(
-      (event, emit) => emit(
-        state.copyWith(
-          memory: state.memory.copyWith(time: event.dateTime),
+        (memory) => state.copyWith(
+          memory: memory,
+          selectedType: memory.memoryContent.type,
         ),
-      ),
-    );
-
-    on<_MemoryTypeChanged>(
-      (event, emit) => emit(
-        state.copyWith(
-          selectedType: event.memoryContentType,
-          memory: Memory.empty(),
-        ),
-      ),
-    );
-
-    on<_TextContentChanged>(
-      (event, emit) => emit(
-        state.copyWith(
-          memory: state.memory.copyWith(
-            memoryContent: MemoryContent.text(MemoryText(event.text)),
-          ),
-        ),
-      ),
-    );
-
-    on<_ImageContentChanged>(
-      (event, emit) => emit(
-        state.copyWith(
-          memory: state.memory.copyWith(
-            memoryContent: MemoryContent.image(
-              MemoryImage(
-                event.imagePath,
-              ),
-              MemoryText(event.caption),
-            ),
-          ),
-        ),
-      ),
-    );
+      ));
+      // TODO: implement Initialized Event handler
+    });
+    on<_DateTimeChanged>((event, emit) {
+      // TODO: implement _DateTimeChanged  event handler
+    });
+    on<_MemoryTypeChanged>((event, emit) {
+      // TODO: implement _MemoryTypeChanged event handler
+    });
+    on<_TextContentChanged>((event, emit) {
+      // TODO: implement _TextContentChanged event handler
+    });
+    on<_ImageContentChanged>((event, emit) {
+      // TODO: implement  _ImageContentChanged  event handler
+    });
+    on<_VoiceContentChanged>((event, emit) {
+      // TODO: implement _VoiceContentChanged event handler
+    });
+    on<_VoiceButtonPressed>((event, emit) {
+      // TODO: implement _VoiceButtonPressed event handler
+    });
+    on<_Saved>((event, emit) {
+      // TODO: implement _Saved event handler
+    });
   }
 }
+// _Initialized
+// 
+// 
+// 
+// 
+// 
+// 
+// 
