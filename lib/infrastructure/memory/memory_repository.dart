b4c317ea116d319dbb577c95c807336aabdec01c
Amal Shaheen <mkamalshaheen@gmail.com>
2025-01-15 19:20:59 +0530

    feat: implement Isar initialization in MemoryRepository and update create method to use MemoryDto [incomplete]

--- lib/infrastructure/memory/memory_repository.dart
+++ lib/infrastructure/memory/memory_repository.dart
@@ -1,14 +1,27 @@
 import 'package:fpdart/fpdart.dart';
 import 'package:injectable/injectable.dart';
+import 'package:isar/isar.dart';
 import 'package:thought_trail/domain/memory/i_memory_repository.dart';
 import 'package:thought_trail/domain/memory/memory.dart';
 import 'package:thought_trail/domain/memory/memory_failure.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:thought_trail/infrastructure/memory/memory_dto.dart';
 
 @LazySingleton(as: IMemoryRepository)
 class MemoryRepository implements IMemoryRepository {
+  Future<Isar> initializeIsar() async {
+    final dir = await getApplicationDocumentsDirectory();
+    if (Isar.instanceNames.isEmpty) {
+      return await Isar.open([MemoryDtoSchema], directory: dir.path);
+    }
+    return Future.value(Isar.getInstance());
+  }
+
   @override
-  Future<Either<MemoryFailure, Unit>> create({required Memory memory}) {
-    // TODO: implement create
+  Future<Either<MemoryFailure, Unit>> create({required Memory memory}) async {
+    final isar = await initializeIsar();
+    final memoryDto = MemoryDto.fromDomain(memory);
+
     throw UnimplementedError();
   }
 
