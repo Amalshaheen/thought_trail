0485afb5575ff36d1db1696688a428ae3de763c9
Amal Shaheen <mkamalshaheen@gmail.com>
2025-01-31 21:03:47 +0530

    feat: enhance AddMemoryFAB with improved state handling and error messaging

--- lib/presentation/timeline/widgets/add_memory_fab.dart
+++ lib/presentation/timeline/widgets/add_memory_fab.dart
@@ -1,9 +1,10 @@
+import 'dart:developer';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:fpdart/fpdart.dart';
 import 'package:thought_trail/application/memory/memory_form/memory_form_bloc.dart';
-import 'package:thought_trail/domain/core/failures.dart';
 import 'package:thought_trail/domain/memory/memory_content.dart';
 import 'package:thought_trail/presentation/timeline/widgets/memory_input_corousel.dart';
 
@@ -15,38 +16,98 @@ class AddMemoryFAB extends StatelessWidget {
   Widget build(BuildContext context) {
     return FloatingActionButton(
       onPressed: () {
+        context.read<MemoryFormBloc>().add(
+              MemoryFormEvent.initialized(
+                none(),
+              ),
+            );
+
         showModalBottomSheet(
           context: context,
-          builder: (context) => BlocBuilder<MemoryFormBloc, MemoryFormState>(
-            builder: (ctx, state) {
-              return Padding(
-                padding: const EdgeInsets.symmetric(vertical: 10.0),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  mainAxisSize: MainAxisSize.min,
-                  spacing: 10,
-                  children: [
-                    Text(
-                      'Add Memory',
-                      style:
-                          Theme.of(context).textTheme.headlineMedium?.copyWith(
-                                color: Theme.of(context).colorScheme.primary,
-                              ),
-                    ),
-                    _buildDatePicker(context, ctx),
-                    MemoryInputCorousel(
-                      onMemoryContentChanged: (content) {
-                        ctx.read<MemoryFormBloc>().add(
-                              MemoryFormEvent.memoryContentChanged(content),
-                            );
-                      },
+          builder: (context) => BlocListener<MemoryFormBloc, MemoryFormState>(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 10.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                mainAxisSize: MainAxisSize.min,
+                spacing: 10,
+                children: [
+                  Text(
+                    'Add Memory',
+                    style: Theme.of(context).textTheme.headlineMedium?.copyWith(
+                          color: Theme.of(context).colorScheme.primary,
+                        ),
+                  ),
+                  _buildDatePicker(context),
+                  MemoryInputCorousel(
+                    onMemoryContentChanged: (content) {
+                      context.read<MemoryFormBloc>().add(
+                            MemoryFormEvent.memoryContentChanged(content),
+                          );
+                    },
                     textController: _textController,
-                    ),
-                    _buildSubmitButton(context, state),
-                    // _handleSubmissionFailureOrSuccess(context, state),
-                  ],
-                ),
-              );
+                  ),
+                  _buildSubmitButton(context),
+                  // _handleSubmissionFailureOrSuccess(context, state),
+                ],
+              ),
+            ),
+            listener: (ctx, state) {
+              state.isProcessing
+                  ? log('Processing')
+                  : state.memory.memoryContent.failureOption.fold(
+                      () {
+                        state.submissionFailureOrSuccessOption.fold(
+                          () {
+                            log('No Submission Failure or Success');
+                          },
+                          (either) {
+                            either.fold(
+                              (memoryFailure) {
+                                state.showErrorMessages
+                                    ? ScaffoldMessenger.of(context)
+                                        .showSnackBar(
+                                        SnackBar(
+                                          content: Text(memoryFailure.map(
+                                            unexpected: (_) =>
+                                                'Unexpected error',
+                                            emptyMemory: (_) =>
+                                                'Empty Entry! try adding your thought',
+                                          )),
+                                        ),
+                                      )
+                                    : {null};
+                              },
+                              (_) {
+                                ScaffoldMessenger.of(context).showSnackBar(
+                                  SnackBar(
+                                    content: Text(
+                                        'Memory added successfully : ${state.memory.memoryContent}'),
+                                  ),
+                                );
+                                _textController.clear();
+                                Navigator.pop(context);
+                              },
+                            );
+                          },
+                        );
+                      },
+                      (valueFailure) {
+                        state.showErrorMessages
+                            ? ScaffoldMessenger.of(context).showSnackBar(
+                                SnackBar(
+                                  content: Text(
+                                    valueFailure.when(
+                                      empty: (_) => 'Memory Content is Empty',
+                                      invalidURL: (_) => 'invalidURL',
+                                      invalidAudioURL: (_) => 'invalidAudioURL',
+                                    ),
+                                  ),
+                                ),
+                              )
+                            : null;
+                      },
+                    );
             },
           ),
         );
@@ -55,7 +116,7 @@ class AddMemoryFAB extends StatelessWidget {
     );
   }
 
-  Widget _buildDatePicker(BuildContext context, BuildContext ctx) {
+  Widget _buildDatePicker(BuildContext context) {
     return SizedBox(
       height: 50,
       child: CupertinoTheme(
@@ -70,7 +131,7 @@ class AddMemoryFAB extends StatelessWidget {
         child: CupertinoDatePicker(
           onDateTimeChanged: (DateTime datetime) {
             if (context.mounted) {
-              ctx.read<MemoryFormBloc>().add(
+              context.read<MemoryFormBloc>().add(
                     MemoryFormEvent.dateTimeChanged(datetime),
                   );
             }
@@ -82,7 +143,7 @@ class AddMemoryFAB extends StatelessWidget {
     );
   }
 
-  Widget _buildSubmitButton(BuildContext context, MemoryFormState state) {
+  Widget _buildSubmitButton(BuildContext context) {
     var style = ElevatedButton.styleFrom(
       backgroundColor: Theme.of(context).colorScheme.primary,
       foregroundColor: Theme.of(context).colorScheme.onPrimary,
@@ -91,62 +152,16 @@ class AddMemoryFAB extends StatelessWidget {
       mainAxisAlignment: MainAxisAlignment.spaceAround,
       children: [
         ElevatedButton(
-          onPressed:
-              state.isProcessing ? null : () => Navigator.of(context).pop(),
+          onPressed: () {
+            _textController.clear();
+            Navigator.of(context).pop();
+          },
           child: const Text('Cancel'),
         ),
         ElevatedButton(
-          onPressed: state.isProcessing
-              ? null
-              : () {
-                  context
-                      .read<MemoryFormBloc>()
-                      .add(MemoryFormEvent.submitted());
-                  state.memory.memoryContent.failureOption.fold(
-                    () {
-                      state.submissionFailureOrSuccessOption.fold(
-                        () {},
-                        (either) {
-                          either.fold(
-                            (memoryFailure) {
-                              ScaffoldMessenger.of(context).showSnackBar(
-                                SnackBar(
-                                  content: Text(memoryFailure.map(
-                                    unexpected: (_) => 'Unexpected error',
-                                    emptyMemory: (_) =>
-                                        'Empty Entry! try adding your thought',
-                                  )),
-                                ),
-                              );
-                            },
-                            (_) {
-                              ScaffoldMessenger.of(context).showSnackBar(
-                                SnackBar(
-                                  content:
-                                      const Text('Memory added successfully'),
-                                ),
-                              );
-                              Navigator.pop(context);
-                            },
-                          );
-                        },
-                      );
-                    },
-                    (valueFailure) {
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        SnackBar(
-                          content: Text(
-                            valueFailure.when(
-                              empty: (_) => 'Memory Content is Empty',
-                              invalidURL: (_) => 'invalidURL',
-                              invalidAudioURL: (_) => 'invalidAudioURL',
-                            ),
-                          ),
-                        ),
-                      );
-                    },
-                  );
-                },
+          onPressed: () async {
+            context.read<MemoryFormBloc>().add(MemoryFormEvent.submitted());
+          },
           style: style,
           child: const Text('Add Memory'),
         ),
