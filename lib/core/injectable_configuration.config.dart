1831e4df2ad60edb30f2d157bbb341e298e3e192
Amal Shaheen <mkamalshaheen@gmail.com>
2025-02-07 22:01:51 +0530

    feat: register MemoryWatcherBloc in the injectable configuration for dependency injection

--- lib/core/injectable_configuration.config.dart
+++ lib/core/injectable_configuration.config.dart
@@ -12,6 +12,8 @@ import 'package:get_it/get_it.dart' as _i1;
 import 'package:injectable/injectable.dart' as _i2;
 import 'package:thought_trail/application/memory/memory_form/memory_form_bloc.dart'
     as _i5;
+import 'package:thought_trail/application/memory/memory_watcher/memory_watcher_bloc.dart'
+    as _i6;
 import 'package:thought_trail/domain/memory/i_memory_repository.dart' as _i3;
 import 'package:thought_trail/infrastructure/memory/memory_repository.dart'
     as _i4;
@@ -30,6 +32,8 @@ extension GetItInjectableX on _i1.GetIt {
     gh.lazySingleton<_i3.IMemoryRepository>(() => _i4.MemoryRepository());
     gh.factory<_i5.MemoryFormBloc>(
         () => _i5.MemoryFormBloc(gh<_i3.IMemoryRepository>()));
+    gh.factory<_i6.MemoryWatcherBloc>(
+        () => _i6.MemoryWatcherBloc(gh<_i3.IMemoryRepository>()));
     return this;
   }
 }
