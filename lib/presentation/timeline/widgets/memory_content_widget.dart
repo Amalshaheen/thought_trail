47612be0cd5fc9237b6d0f3affb0e71963702eae
Amal Shaheen <mkamalshaheen@gmail.com>
2025-02-12 11:57:19 +0530

    feat: implement memory content rendering for text, image, and voice types in MemoryContentWidget

--- lib/presentation/timeline/widgets/memory_content_widget.dart
+++ lib/presentation/timeline/widgets/memory_content_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:thought_trail/domain/core/error.dart';
 import 'package:thought_trail/domain/memory/memory_content.dart';
 
 class MemoryContentWidget extends StatelessWidget {
@@ -12,38 +13,41 @@ class MemoryContentWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // return Text('${memory.memory!.text}');
-    return SizedBox(
-      height: 50,
-    );
-
-    // memoryContent.map(
-    //   text: (text) => Text(
-    //     text.text,
-    //   ),
-    //   image: (image) => Padding(
-    //     padding: const EdgeInsets.all(8.0),
-    //     child: Image.network(
-    //       height: 200,
-    //       image.imagePath,
-    //       frameBuilder: (context, child, frame, wasSynchronouslyLoaded) {
-    //         // print('image loaded');
-    //         if (wasSynchronouslyLoaded) {
-    //           return child;
-    //         } else {
-    //           return AnimatedOpacity(
-    //             opacity: frame == null ? 0 : 1,
-    //             duration: const Duration(seconds: 1),
-    //             curve: Curves.easeOut,
-    //             child: child,
-    //           );
-    //         }
-    //       },
-    //     ),
-    //   ),
-    //   audio: (value) => Container(
-    //     height: 50,
-    //     color: Colors.red[300],
-    //   ),
+    // return SizedBox(
+    //   height: 50,
     // );
+
+    return memoryContent.map(
+      text: (text) => Text(
+        text.text.value.getOrCrash(),
+      ),
+      image: (image) => Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Image.network(
+          height: 200,
+          image.image.value.getOrCrash(),
+          frameBuilder: (context, child, frame, wasSynchronouslyLoaded) {
+            // print('image loaded');
+            if (wasSynchronouslyLoaded) {
+              return child;
+            } else {
+              return AnimatedOpacity(
+                opacity: frame == null ? 0 : 1,
+                duration: const Duration(seconds: 1),
+                curve: Curves.easeOut,
+                child: child,
+              );
+            }
+          },
+        ),
+      ),
+      voice: (value) => Container(
+        height: 50,
+        color: Colors.red[300],
+      ),
+      none: (value) {
+        return const SizedBox();
+      },
+    );
   }
 }
