b84063c1e2db3dd05fd00f78164800fbfaba6403
Amal Shaheen <mkamalshaheen@gmail.com>
2025-02-12 11:58:01 +0530

    feat: enhance MemoriesListWidget with MemoryListSuccessWidget for improved memory display and scrolling

--- lib/presentation/timeline/widgets/memories_list_widget.dart
+++ lib/presentation/timeline/widgets/memories_list_widget.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:thought_trail/application/memory/memory_watcher/memory_watcher_bloc.dart';
+import 'package:thought_trail/domain/memory/memory.dart';
 import 'package:thought_trail/presentation/timeline/widgets/memory_list_tile_widget.dart';
 
 class MemoriesListWidget extends StatelessWidget {
@@ -12,26 +13,55 @@ class MemoriesListWidget extends StatelessWidget {
       child: BlocBuilder<MemoryWatcherBloc, MemoryWatcherState>(
         builder: (context, state) {
           return state.map(
-            initial: (_) => Container(
-              child: Text('initial'),
-            ),
+            initial: (_) => Text('initial'),
             loadInProgress: (_) => const Center(
               child: CircularProgressIndicator(),
             ),
-            loadSuccess: (state) => ListView.builder(
-              itemCount: state.memories.length,
-              itemBuilder: (context, index) {
-                return MemoryListTileWidget(
-                  memory: state.memories[index],
-                );
-              },
-            ),
+            loadSuccess: (state) {
+              final memories = state.memories;
+              return SizedBox(
+                height: MediaQuery.of(context).size.height,
+                child: MemoryListSuccessWidget(memories: memories),
+              );
+            },
             loadFailure: (_) => const Center(
               child: Text('Something went wrong'),
             ),
           );
         },
       ),
+    );
+  }
+}
+
+class MemoryListSuccessWidget extends StatelessWidget {
+  MemoryListSuccessWidget({
+    super.key,
+    required this.memories,
+  });
+
+  final List<Memory> memories;
+  final ScrollController scrollController = ScrollController();
+
+  @override
+  Widget build(BuildContext context) {
+    WidgetsBinding.instance.addPostFrameCallback(
+      (_) {
+        scrollController.animateTo(
+          scrollController.position.maxScrollExtent,
+          duration: Duration(milliseconds: 500),
+          curve: Curves.easeOut,
+        );
+      },
+    );
+    return ListView.builder(
+      controller: scrollController,
+      itemCount: memories.length,
+      itemBuilder: (context, index) {
+        return MemoryListTileWidget(
+          memory: memories[index],
+        );
+      },
     );
   }
 }
