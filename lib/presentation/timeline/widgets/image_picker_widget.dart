cbbcad881643d6210f71e8e2ff451313f22d5733
Amal Shaheen <mkamalshaheen@gmail.com>
2025-02-15 22:53:13 +0530

    refactor: simplify ImagePickerWidget by removing unnecessary BlocProvider

--- lib/presentation/timeline/widgets/image_picker_widget.dart
+++ lib/presentation/timeline/widgets/image_picker_widget.dart
@@ -14,65 +14,61 @@ class ImagePickerWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) =>
-          getit<ImagePickerBloc>()..add(const ImagePickerEvent.started()),
-      child: BlocBuilder<ImagePickerBloc, ImagePickerState>(
-        builder: (context, state) {
-          return state.map(
-            initial: (_) => SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
-              child: TextButton.icon(
-                onPressed: () {
-                  context
-                      .read<ImagePickerBloc>()
-                      .add(const ImagePickerEvent.pickImage());
-                },
-                label: const Text(
-                  'Upload Image',
-                  softWrap: false,
-                  overflow: TextOverflow.ellipsis,
-                  maxLines: 1,
-                ),
-                icon: const Icon(Icons.image),
+    return BlocBuilder<ImagePickerBloc, ImagePickerState>(
+      builder: (context, state) {
+        return state.map(
+          initial: (_) => SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: TextButton.icon(
+              onPressed: () {
+                context
+                    .read<ImagePickerBloc>()
+                    .add(const ImagePickerEvent.pickImage());
+              },
+              label: const Text(
+                'Upload Image',
+                softWrap: false,
+                overflow: TextOverflow.ellipsis,
+                maxLines: 1,
               ),
+              icon: const Icon(Icons.image),
             ),
-            loading: (_) => const CircularProgressIndicator(),
-            success: (state) {
-              log('Image path: ${state.image.value.getOrCrash()}');
-              context
-                  .read<MemoryFormBloc>()
-                  .add(MemoryFormEvent.memoryContentChanged(
-                    MemoryContent.image(state.image, none()), //TODO:caption
-                  ));
-              return Stack(
-                children: [
-                  Image.file(
-                    File(state.image.value.getOrCrash()),
-                    height: 200,
-                  ),
-                  Positioned(
-                    top: 0,
-                    right: 0,
-                    child: IconButton(
-                      icon: const Icon(
-                        Icons.close,
-                        color: Colors.grey,
-                      ),
-                      onPressed: () {
-                        context
-                            .read<ImagePickerBloc>()
-                            .add(const ImagePickerEvent.canceledByUser());
-                      },
+          ),
+          loading: (_) => const CircularProgressIndicator(),
+          success: (state) {
+            log('Image path: ${state.image.value.getOrCrash()}');
+            context
+                .read<MemoryFormBloc>()
+                .add(MemoryFormEvent.memoryContentChanged(
+                  MemoryContent.image(state.image, none()), //TODO:caption
+                ));
+            return Stack(
+              children: [
+                Image.file(
+                  File(state.image.value.getOrCrash()),
+                  height: 200,
+                ),
+                Positioned(
+                  top: 0,
+                  right: 0,
+                  child: IconButton(
+                    icon: const Icon(
+                      Icons.close,
+                      color: Colors.grey,
                     ),
+                    onPressed: () {
+                      context
+                          .read<ImagePickerBloc>()
+                          .add(const ImagePickerEvent.canceledByUser());
+                    },
                   ),
-                ],
-              );
-            },
-            failure: (state) => Text('Error: ${state.failure}'),
-          );
-        },
-      ),
+                ),
+              ],
+            );
+          },
+          failure: (state) => Text('Error: ${state.failure}'),
+        );
+      },
     );
   }
 }
