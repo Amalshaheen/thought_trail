eff7fddeb5ce9b03c7a0eec348ba87c827453e62
Amal Shaheen <mkamalshaheen@gmail.com>
2025-02-27 21:43:35 +0530

    feat: add AudioPlayerWidget for voice playback control and enhance error handling in VoiceRecorder

--- lib/presentation/timeline/widgets/voice_recorder.dart
+++ lib/presentation/timeline/widgets/voice_recorder.dart
@@ -1,6 +1,6 @@
-import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:thought_trail/application/memory/voice_player/voice_player_bloc.dart';
 import 'package:thought_trail/application/memory/voice_recorder/voice_recorder_bloc.dart';
 import 'package:thought_trail/domain/voice/voice_object.dart';
 
@@ -96,11 +96,82 @@ class VoiceRecorder extends StatelessWidget {
               ),
             ],
           ),
-          recordingStopped: (memoryVoice) => Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [],
-          ),
+          recordingStopped: (memoryVoice) => AudioPlayerWidget(),
           error: (message) => Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Icon(Icons.error, color: Colors.red),
+              const SizedBox(height: 8),
+              Text(message.toString()),
+            ],
+          ),
+        );
+      },
+    );
+  }
+}
+
+class AudioPlayerWidget extends StatelessWidget {
+  const AudioPlayerWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<VoicePlayerBloc, VoicePlayerState>(
+      builder: (context, state) {
+        return state.when(
+          initial: () => IconButton(
+            icon: const Icon(Icons.play_arrow),
+            onPressed: () {
+              context
+                  .read<VoicePlayerBloc>()
+                  .add(VoicePlayerEvent.played(MemoryVoice('')));
+            },
+          ),
+          playing: (voice) => Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Icon(Icons.music_note, color: Colors.green),
+              const SizedBox(height: 8),
+              const Text('Playing...'),
+              const SizedBox(height: 8),
+              IconButton(
+                icon: const Icon(Icons.pause),
+                onPressed: () {
+                  context
+                      .read<VoicePlayerBloc>()
+                      .add(VoicePlayerEvent.paused(voice));
+                },
+              ),
+            ],
+          ),
+          paused: (voice) => Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Icon(Icons.music_note, color: Colors.grey),
+              const SizedBox(height: 8),
+              const Text('Paused'),
+              const SizedBox(height: 8),
+              IconButton(
+                icon: const Icon(Icons.play_arrow),
+                onPressed: () {
+                  context
+                      .read<VoicePlayerBloc>()
+                      .add(VoicePlayerEvent.played(voice));
+                },
+              ),
+            ],
+          ),
+          stopped: (voice) => IconButton(
+            icon: const Icon(Icons.play_arrow),
+            onPressed: () {
+              context
+                  .read<VoicePlayerBloc>()
+                  .add(VoicePlayerEvent.played(voice));
+            },
+          ),
+          failure: (message) => Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               const Icon(Icons.error, color: Colors.red),
