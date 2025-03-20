5df914d83414afe36eb0b3fe07695122bdc025e3
Amal Shaheen <mkamalshaheen@gmail.com>
2025-03-20 09:13:23 +0530

    feat: enhance MemoryContentWidget to support dynamic image sizing and add image_size_getter dependency

--- lib/presentation/timeline/widgets/memory_content_widget.dart
+++ lib/presentation/timeline/widgets/memory_content_widget.dart
@@ -1,14 +1,16 @@
 import 'dart:io';
+import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:image_size_getter/file_input.dart';
+import 'package:image_size_getter/image_size_getter.dart';
 import 'package:thought_trail/application/memory/voice_player/voice_player_bloc.dart';
 import 'package:thought_trail/core/injectable_configuration.dart';
 import 'package:thought_trail/domain/core/error.dart';
 import 'package:thought_trail/domain/memory/memory_content.dart';
 import 'package:thought_trail/presentation/timeline/widgets/audio_player_widget.dart';
-import 'package:thought_trail/presentation/timeline/widgets/voice_recorder.dart';
 
 class MemoryContentWidget extends StatelessWidget {
   const MemoryContentWidget({
@@ -36,7 +38,7 @@ class MemoryContentWidget extends StatelessWidget {
           ),
         ),
         image: (image) => MemoryImageWidget(
-          image: image.image.value.getOrCrash(),
+          imageUrl: image.image.value.getOrCrash(),
         ),
         voice: (voice) => BlocProvider(
           create: (context) => getit<VoicePlayerBloc>(),
@@ -53,36 +55,39 @@ class MemoryContentWidget extends StatelessWidget {
 }
 
 class MemoryImageWidget extends StatelessWidget {
-  const MemoryImageWidget({
-    super.key,
-    required this.image,
-  });
-  final String image;
+  const MemoryImageWidget({super.key, required this.imageUrl});
+  final String imageUrl;
   @override
   Widget build(BuildContext context) {
-    return ConstrainedBox(
-      constraints: BoxConstraints(
-        maxHeight: 500,
-      ),
-      child: ClipRRect(
-        borderRadius: BorderRadius.circular(10),
+    final image = File(imageUrl);
+    final originalSize = ImageSizeGetter.getSizeResult(FileInput(image)).size;
+    final originalHeight = originalSize.height;
+    final originalWidth = originalSize.width;
+
+    const maxHeight = 500.0;
+    const maxWidth = 300.0;
+
+    double height = originalHeight.toDouble();
+
+    if (originalHeight > maxHeight || originalWidth > maxWidth) {
+      if (originalHeight / maxHeight > originalWidth / maxWidth) {
+        height = maxHeight;
+      } else {
+        height = originalHeight * (maxWidth / originalWidth);
+      }
+    }
+    // final height = ImageSizeGetter.getSizeResult(FileInput(image)).size.height;
+
+    return SizedBox(
+      height: min(height.toDouble(), 500),
+
+      // width: double.infinity,
+      child: Card(
+        // borderRadius: BorderRadius.circular(10),
+        elevation: 5,
         child: Image.file(
+          image,
           fit: BoxFit.cover,
-          File(image),
-          // height: 200,
-          frameBuilder: (context, child, frame, wasSynchronouslyLoaded) {
-            // print('image loaded');
-            if (wasSynchronouslyLoaded) {
-              return child;
-            } else {
-              return AnimatedOpacity(
-                opacity: frame == null ? 0 : 1,
-                duration: const Duration(seconds: 1),
-                curve: Curves.easeOut,
-                child: child,
-              );
-            }
-          },
         ),
       ),
     );
