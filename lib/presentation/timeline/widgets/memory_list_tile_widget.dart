2b5764af8b17c24652a8c483ee2ae1e06d592d69
Amal Shaheen <mkamalshaheen@gmail.com>
2025-04-11 20:21:50 +0530

    refactor: streamline MemoryListTileWidget layout and remove unused variables

--- lib/presentation/timeline/widgets/memory_list_tile_widget.dart
+++ lib/presentation/timeline/widgets/memory_list_tile_widget.dart
@@ -3,7 +3,6 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:thought_trail/application/memory/memory_form/memory_form_bloc.dart';
 import 'package:thought_trail/domain/core/error.dart';
 import 'package:thought_trail/domain/memory/memory.dart';
-import 'package:thought_trail/domain/memory/memory_content.dart';
 import 'package:thought_trail/presentation/timeline/widgets/memory_content_widget.dart';
 import 'package:thought_trail/presentation/timeline/widgets/memory_time_widget.dart';
 import 'package:thought_trail/presentation/timeline/widgets/memory_timeline_widget.dart';
@@ -28,102 +27,60 @@ class MemoryListTileWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // Check if the memory is from today
-    final bool isToday = memory.time.day == DateTime.now().day &&
-        memory.time.month == DateTime.now().month &&
-        memory.time.year == DateTime.now().year;
-
-    // Check if the memory has a content
-    final bool isMemory = memory.memoryContent != MemoryContent.none();
-
-    // Check if the time is one of 6am, 12pm, 4pm, or 8pm
-    final bool isPerfectTime =
-        {6, 12, 16, 20}.contains(memory.time.hour) && memory.time.minute == 0;
-
-    // The text label for the time
-    const timeLabel = {
-      6: 'morning',
-      12: 'afternoon',
-      16: 'evening',
-      20: 'night',
-    };
-
     return IntrinsicHeight(
       child: Row(
-        // direction: Axis.horizontal,
         crossAxisAlignment: CrossAxisAlignment.center,
-
-        // mainAxisAlignment: MainAxisAlignment.spaceAround,
-
         children: [
           Expanded(
-            // width: double.infinity,
             child: ConstrainedBox(
               constraints: BoxConstraints(
-                // The minimum height is 100 if the memory is from today or
-                // if the memory has a content. Otherwise, the minimum height
-                // is 0.
-                minHeight: (isToday || isMemory) ? 100 : 0,
+                minHeight: 100,
               ),
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  // If the memory is from today and the time is one of 6am,
-                  // 12pm, 4pm, or 8pm, display the time label.
-                  (isToday && isPerfectTime || isMemory && isPerfectTime)
-                      ? Text('${timeLabel[memory.time.hour]}')
-                      : SizedBox(),
-                  // If the memory has a content, display the content.
-                  (isMemory)
-                      ? Dismissible(
-                          key: Key(memory.id.value.getOrCrash()),
-                          direction: DismissDirection.endToStart,
-                          confirmDismiss: (direction) async {
-                            final snackBarController =
-                                ScaffoldMessenger.of(context).showSnackBar(
-                              SnackBar(
-                                content: AnimatedDismissibleBackground(),
-                                action: SnackBarAction(
-                                  label: "Undo",
-                                  onPressed: () {
-                                    ScaffoldMessenger.of(context)
-                                        .hideCurrentSnackBar(
-                                      reason: SnackBarClosedReason.action,
-                                    );
-                                  },
-                                ),
-                              ),
-                            );
-                            Future.delayed(Duration(seconds: 4),
-                                () => snackBarController.close());
+                  Dismissible(
+                      key: Key(memory.id.value.getOrCrash()),
+                      direction: DismissDirection.endToStart,
+                      confirmDismiss: (direction) async {
+                        final snackBarController =
+                            ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(
+                            content: AnimatedDismissibleBackground(),
+                            action: SnackBarAction(
+                              label: "Undo",
+                              onPressed: () {
+                                ScaffoldMessenger.of(context)
+                                    .hideCurrentSnackBar(
+                                  reason: SnackBarClosedReason.action,
+                                );
+                              },
+                            ),
+                          ),
+                        );
+                        Future.delayed(Duration(seconds: 4),
+                            () => snackBarController.close());
 
-                            if (await snackBarController.closed ==
-                                SnackBarClosedReason.action) {
-                              return false;
-                            } else {
-                              return true;
-                            }
-                          },
-                          onDismissed: (direction) {
-                            context
-                                .read<MemoryFormBloc>()
-                                .add(MemoryFormEvent.deleted(memory));
-                          },
-                          child: MemoryContentWidget(
-                              memoryContent: memory.memoryContent))
-                      : SizedBox(),
+                        if (await snackBarController.closed ==
+                            SnackBarClosedReason.action) {
+                          return false;
+                        } else {
+                          return true;
+                        }
+                      },
+                      onDismissed: (direction) {
+                        context
+                            .read<MemoryFormBloc>()
+                            .add(MemoryFormEvent.deleted(memory));
+                      },
+                      child: MemoryContentWidget(
+                          memoryContent: memory.memoryContent))
                 ],
               ),
             ),
           ),
-          // Spacer(),
-          // If the memory is from today or has a content, display the timeline
-          // and time widgets.
-          (isToday || isMemory) ? MemoryTimelineWidget() : SizedBox(),
-          (isToday || isMemory)
-              // Display the time widget
-              ? SizedBox(width: 50, child: MemoryTimeWidget(time: memory.time))
-              : SizedBox(),
+          MemoryTimelineWidget(),
+          SizedBox(width: 40, child: MemoryTimeWidget(time: memory.time)),
         ],
       ),
     );
