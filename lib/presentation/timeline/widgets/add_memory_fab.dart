805ec995fe51341cc5c1c99b672ff99ceaeac528
Amal Shaheen <mkamalshaheen@gmail.com>
2025-04-11 20:19:27 +0530

    refactor: refactor AddMemoryFAB to improve code structure and readability by converting DatePicker and SubmitButton fucntions to  widgets

--- lib/presentation/timeline/widgets/add_memory_fab.dart
+++ lib/presentation/timeline/widgets/add_memory_fab.dart
@@ -11,123 +11,139 @@ import 'package:thought_trail/domain/memory/memory_content.dart';
 import 'package:thought_trail/presentation/timeline/widgets/memory_input_corousel.dart';
 
 class AddMemoryFAB extends StatelessWidget {
-  final TextEditingController _textController = TextEditingController(text: '');
+  final TextEditingController textController = TextEditingController(text: '');
   AddMemoryFAB({super.key});
 
   @override
   Widget build(BuildContext context) {
     return FloatingActionButton(
       onPressed: () {
-        context.read<MemoryFormBloc>().add(
-              MemoryFormEvent.initialized(
-                none(),
-              ),
-            );
-
-        showModalBottomSheet(
-          showDragHandle: true,
-          isScrollControlled: true,
-          context: context,
-          builder: (context) => BlocListener<MemoryFormBloc, MemoryFormState>(
-            child: SingleChildScrollView(
-              child: Padding(
-                padding: EdgeInsets.only(
-                    top: 10.0,
-                    bottom: MediaQuery.of(context).viewInsets.bottom),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  mainAxisSize: MainAxisSize.min,
-                  spacing: 15,
-                  children: [
-                    Text(
-                      'Add Memory',
-                      style:
-                          Theme.of(context).textTheme.headlineMedium?.copyWith(
-                                color: Theme.of(context).colorScheme.primary,
-                              ),
-                    ),
-                    _buildDatePicker(context),
-                    MemoryInputCorousel(
-                      onMemoryContentChanged: (content) {
-                        context.read<MemoryFormBloc>().add(
-                              MemoryFormEvent.memoryContentChanged(content),
-                            );
-                      },
-                      textController: _textController,
-                    ),
-                    _buildSubmitButton(context),
-                    // _handleSubmissionFailureOrSuccess(context, state),
-                    AnimatedSwitcher(
-                      duration: const Duration(milliseconds: 300),
-                      child: context.read<MemoryFormBloc>().state.isProcessing
-                          ? const LinearProgressIndicator()
-                          : const SizedBox.shrink(),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-            listener: (ctx, state) {
-              state.isProcessing
-                  ? log('Processing')
-                  : state.memory.memoryContent.failureOption.fold(
-                      () {
-                        state.submissionFailureOrSuccessOption.fold(
-                          () {
-                            log('No Submission Failure or Success');
-                          },
-                          (either) {
-                            either.fold(
-                              (memoryFailure) {
-                                state.showErrorMessages
-                                    ? Fluttertoast.showToast(
-                                        msg: memoryFailure.map(
-                                        unexpected: (_) => 'Unexpected error',
-                                        emptyMemory: (_) =>
-                                            'Empty Entry! try adding your thought',
-                                      ))
-                                    : {null};
-                              },
-                              (_) {
-                                Fluttertoast.showToast(
-                                  msg: 'Memory Added',
-                                );
-                                _textController.clear();
-                                context.read<MemoryWatcherBloc>().add(
-                                      MemoryWatcherEvent.memoryUpdated(
-                                          state.memory),
-                                    );
-
-                                Navigator.pop(context);
-                              },
-                            );
-                          },
-                        );
-                      },
-                      (valueFailure) {
-                        state.showErrorMessages
-                            ? Fluttertoast.showToast(
-                                msg: valueFailure.when(
-                                  empty: (_) => 'Memory Content is Empty',
-                                  invalidURL: (_) => 'invalidURL',
-                                  invalidAudioFormat: (_) => 'invalidAudioURL',
-                                  invalidFilePath: (_) => 'invalidFilePath',
-                                  invalidImageFormat: (_) =>
-                                      'Invalid file format',
-                                ),
-                              )
-                            : null;
-                      },
-                    );
-            },
-          ),
-        );
+        addMemoryBottomSheet(context, textController);
       },
       child: const Icon(Icons.add),
     );
   }
 
-  Widget _buildDatePicker(BuildContext context) {
+  static void addMemoryBottomSheet(
+      BuildContext context, TextEditingController textController) {
+    showModalBottomSheet(
+      showDragHandle: true,
+      isScrollControlled: true,
+      context: context,
+      builder: (context) {
+        context.read<MemoryFormBloc>().add(
+              MemoryFormEvent.initialized(
+                none(),
+              ),
+            );
+        return BlocListener<MemoryFormBloc, MemoryFormState>(
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: EdgeInsets.only(
+                  top: 10.0, bottom: MediaQuery.of(context).viewInsets.bottom),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                mainAxisSize: MainAxisSize.min,
+                spacing: 15,
+                children: [
+                  Text(
+                    'Add Memory',
+                    style: Theme.of(context).textTheme.headlineMedium?.copyWith(
+                          color: Theme.of(context).colorScheme.primary,
+                        ),
+                  ),
+                  DatePicker(context: context),
+                  MemoryInputCorousel(
+                    onMemoryContentChanged: (content) {
+                      context.read<MemoryFormBloc>().add(
+                            MemoryFormEvent.memoryContentChanged(content),
+                          );
+                    },
+                    textController: textController,
+                  ),
+                  SubmitButton(
+                    textController: textController,
+                  ),
+                  // _handleSubmissionFailureOrSuccess(context, state),
+                  AnimatedSwitcher(
+                    duration: const Duration(milliseconds: 300),
+                    child: context.read<MemoryFormBloc>().state.isProcessing
+                        ? const LinearProgressIndicator()
+                        : const SizedBox.shrink(),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          listener: (ctx, state) {
+            state.isProcessing
+                ? log('Processing')
+                : state.memory.memoryContent.failureOption.fold(
+                    () {
+                      state.submissionFailureOrSuccessOption.fold(
+                        () {
+                          log('No Submission Failure or Success');
+                        },
+                        (either) {
+                          either.fold(
+                            (memoryFailure) {
+                              state.showErrorMessages
+                                  ? Fluttertoast.showToast(
+                                      msg: memoryFailure.map(
+                                      unexpected: (_) => 'Unexpected error',
+                                      emptyMemory: (_) =>
+                                          'Empty Entry! try adding your thought',
+                                    ))
+                                  : {null};
+                            },
+                            (_) {
+                              Fluttertoast.showToast(
+                                msg: 'Memory Added',
+                              );
+                              textController.clear();
+                              context.read<MemoryWatcherBloc>().add(
+                                    MemoryWatcherEvent.memoryUpdated(
+                                        state.memory),
+                                  );
+
+                              Navigator.pop(context);
+                            },
+                          );
+                        },
+                      );
+                    },
+                    (valueFailure) {
+                      state.showErrorMessages
+                          ? Fluttertoast.showToast(
+                              msg: valueFailure.when(
+                                empty: (_) => 'Memory Content is Empty',
+                                invalidURL: (_) => 'invalidURL',
+                                invalidAudioFormat: (_) => 'invalidAudioURL',
+                                invalidFilePath: (_) => 'invalidFilePath',
+                                invalidImageFormat: (_) =>
+                                    'Invalid file format',
+                              ),
+                            )
+                          : null;
+                    },
+                  );
+          },
+        );
+      },
+    );
+  }
+}
+
+class DatePicker extends StatelessWidget {
+  const DatePicker({
+    super.key,
+    required this.context,
+  });
+
+  final BuildContext context;
+
+  @override
+  Widget build(BuildContext context) {
     return SizedBox(
       height: 50,
       child: CupertinoTheme(
@@ -153,8 +169,18 @@ class AddMemoryFAB extends StatelessWidget {
       ),
     );
   }
+}
 
-  Widget _buildSubmitButton(BuildContext context) {
+class SubmitButton extends StatelessWidget {
+  const SubmitButton({
+    super.key,
+    required this.textController,
+  });
+
+  final TextEditingController textController;
+
+  @override
+  Widget build(BuildContext context) {
     var style = ElevatedButton.styleFrom(
       backgroundColor: Theme.of(context).colorScheme.primary,
       foregroundColor: Theme.of(context).colorScheme.onPrimary,
@@ -164,7 +190,7 @@ class AddMemoryFAB extends StatelessWidget {
       children: [
         ElevatedButton(
           onPressed: () {
-            _textController.clear();
+            textController.clear();
             Navigator.of(context).pop();
           },
           child: const Text('Cancel'),
